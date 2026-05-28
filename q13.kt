f6f08fc4f5585692b24cd06abb38993e44ceab4d
DebasiPattanaik <161007321+DebasiPattanaik@users.noreply.github.com>
2026-05-28 15:55:23 +0530

    Calculate simple interest

---
+++ q13.kt
@@ -0,0 +1,8 @@
+fun main() 
+{
+    var principle = 10000
+    var rate = 5 //% per annum
+    var time = 36 //in months
+    var SI = (principle*rate*time)/100
+    println("Simple Interest = Rs.$SI")
+}
