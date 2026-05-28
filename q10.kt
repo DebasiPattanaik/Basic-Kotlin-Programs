a3905fed70c870ccbf90fea2aeef52cd2de37113
DebasiPattanaik <161007321+DebasiPattanaik@users.noreply.github.com>
2026-05-28 15:38:19 +0530

    Check if a character is vowel or consonant

---
+++ q10.kt
@@ -0,0 +1,12 @@
+fun main() 
+{
+    var ch = 'd'
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        println("Vowel")
+    }
+    else
+    {
+        println("Consonant")
+    }
+}
