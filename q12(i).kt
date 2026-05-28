f9465430a58a761862b76693b5a00b48f0bfd56a
DebasiPattanaik <161007321+DebasiPattanaik@users.noreply.github.com>
2026-05-28 15:45:09 +0530

    Swap 2 numbers using 3rd variable

---
+++ q12(i).kt
@@ -0,0 +1,12 @@
+fun main() 
+{
+    var a = 10
+    var b = 20
+    println("Before Swapping")
+    println("a=$a  b=$b")
+    var temp=a
+    a=b
+    b=temp
+    println("After Swapping")
+    println("a=$a  b=$b")
+}
